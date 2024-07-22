1ff524069f06c842b8e4c42a24c822222964f544
Shahria Mohammed <shmohamm@lab1r3s8.42abudhabi.ae>
2024-07-22 14:10:51 +0400

    Bismi'Allah, first commit, hope to finish by the end of July

---
+++ Makefile
@@ -0,0 +1,48 @@
+RESET		= \033[0m
+GREEN		= \033[32m
+YELLOW		= \033[33m
+
+NAME = cub3d
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -fsanitize=address -g3
+FRAMEWORKS = -framework OpenGL -framework AppKit
+LIB_DIR = libft
+MLX_DIR = mlx
+SRCS = cub3d.c
+OBJS = $(SRCS:.c=.o)
+
+$(NAME): $(LIB_DIR)/libft.a $(MLX_DIR)/libmlx.a $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) $(LIB_DIR)/libft.a -o $(NAME) $(FRAMEWORKS) -L$(MLX_DIR) -lmlx
+	@echo ""
+	@echo "$(GREEN)"
+	@echo "Created: $(words $(OBJS)) object file(s) for Cub3d"
+	@echo "Created: $(NAME)"
+
+$(MLX_DIR)/libmlx.a:
+	@$(MAKE) -C $(MLX_DIR)
+
+$(LIB_DIR)/libft.a:
+	@$(MAKE) -C $(LIB_DIR)
+
+%.o: %.c
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+all: $(NAME)
+
+clean:
+	@rm -f $(OBJS)
+	@$(MAKE) -C $(MLX_DIR) clean
+	@$(MAKE) -C $(LIB_DIR) clean
+
+fclean: clean
+	@rm -f $(NAME)
+	@rm -f $(MLX_DIR)/libmlx.a
+	@rm -f $(LIB_DIR)/libft.a
+	@echo ""
+	@echo "$(YELLOW)"
+	@echo "Removed: $(words $(OBJS)) object file(s) for Cub3d"
+	@echo "Removed: $(NAME)"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ cub3d.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shmohamm <shmohamm@student.42abudhabi.a    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 13:18:00 by shmohamm          #+#    #+#             */
+/*   Updated: 2024/07/22 13:42:08 by shmohamm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdbool.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <math.h>
+# include "libft/libft.h"
+# include "mlx/mlx.h"
+
+# define WINDOW_SIZE_X 800
+# define WINDOW_SIZE_Y 800
+
+#endif
